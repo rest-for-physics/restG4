f5da565cce506963b432de52bf49af80385bdd3f
j.a.galan.81@gmail.com <j.a.galan.81@gmail.com>
2021-03-11 17:31:07 +0100

    Adding few timestamp tests inside 06.IonRecoil validation

--- examples/06.IonRecoils/Validate.C
+++ examples/06.IonRecoils/Validate.C
@@ -15,6 +15,24 @@ Int_t Validate(string fname) {
         return 14;
     }
 
+    Double_t duration = run->GetEndTimestamp() - run->GetStartTimestamp();
+
+    cout << "Duration : " << duration << endl;
+    if (duration <= 5 || duration > 200) {
+        cout << "The duration of the run in seconds is invalid" << endl;
+        return 15;
+    }
+
+    cout.precision(12);
+
+    cout << "Time since simulation ended : " << time(NULL) - run->GetEndTimeStamp() << endl;
+    Double_t delay = time(NULL) - run->GetEndTimeStamp();
+
+    if (delay < 0 || delay > 20) {
+        cout << "The end timestamp is probably wrong" << endl;
+        return 16;
+    }
+
     cout << "All tests passed! [\033[32mOK\033[0m]\n";
     return 0;
 }
