ae8545373c278cb2ee11934e2cf88bc1eec8aa7c
lobis <luis.antonio.obis@gmail.com>
2021-03-27 22:11:11 +0100

    DetectorConstruction.cc: Instead of throwing error at first, now we try to associate unequivocally a logical volume to a physical volume. This is a workaround for assembly support (#3).

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -1,5 +1,7 @@
 #include "DetectorConstruction.hh"
 
+#include <G4LogicalVolumeStore.hh>
+#include <G4PhysicalVolumeStore.hh>
 #include <G4UserLimits.hh>
 
 #include "G4FieldManager.hh"
@@ -50,23 +52,60 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
 
     // TODO : Take the name of the sensitive volume and use it here to define its
     // StepSize
-    string SensVol = (string)restG4Metadata->GetSensitiveVolume();
+    string sensibleVolumeName = (string)restG4Metadata->GetSensitiveVolume();
     bool foundSensitiveVolume = false;
-    for (size_t i = 0; i < restG4Metadata->GetNumberOfActiveVolumes(); i++) {
-        string volume = (string)restG4Metadata->GetActiveVolumeName(i);
-        if (volume == SensVol) {
+    G4PhysicalVolumeStore* physicalVolumeStore = G4PhysicalVolumeStore::GetInstance();
+    cout << "Physical volumes (Geant4)" << endl;
+    for (const auto& physicalVolume : *physicalVolumeStore) {
+        cout << "\t" << (string)physicalVolume->GetName() << endl;
+        if ((string)physicalVolume->GetName() == sensibleVolumeName) {
             foundSensitiveVolume = true;
             break;
         }
     }
+
     if (!foundSensitiveVolume) {
-        G4cout << "RESTG4 error. Sensitive volume " << SensVol << " does not exist in geometry!!" << G4endl;
-        G4cout << "RESTG4 error. Please, review geometry! Press a key to crash!!" << G4endl;
-        getchar();
-        // We need to produce a clean exit at this point
-        exit(1);
+        // If using GDML assembly, the names of G4VPhysicalVolume and GDML physical volume do not match, and I
+        // have found no way to recover the GDML physical volume name using this parser (if you find one,
+        // PLEASE update / write an issue / pull request)
+        //
+        // one workaround is using the logical volume name as an identifier of the physical volume, this works
+        // if the logical volume is only used in one physical volume
+
+        // do an attempt to match logical volume to physical volume before exiting
+
+        map<string, string> physicalToLogicalMap;
+        for (const auto& physicalVolume : *physicalVolumeStore) {
+            physicalToLogicalMap[physicalVolume->GetName()] = physicalVolume->GetLogicalVolume()->GetName();
+        }
+
+        int count = 0;
+        string sensitivePhysicalVolumeName;
+        for (auto const& kv : physicalToLogicalMap) {
+            if (kv.second == sensibleVolumeName) {
+                count++;
+                sensitivePhysicalVolumeName = kv.first;
+            }
+        }
+
+        if (count == 1) {
+            // this means the logical volume only appeared once, so we can safely get the physical volume name
+            cout << "WARNING: Getting the physical volume name ('" << sensitivePhysicalVolumeName
+                 << "') from LOGICAL VOLUME name: " << sensibleVolumeName << endl;
+            cout << "WARNING: Changing RestG4Metadata Sensitive Volume from '" << sensibleVolumeName
+                 << "' to: '" << sensitivePhysicalVolumeName << "'" << endl;
+            restG4Metadata->SetSensitiveVolume((TString)sensitivePhysicalVolumeName);
+            sensibleVolumeName = (string)restG4Metadata->GetSensitiveVolume();
+        } else {
+            G4cout << "RESTG4 error. Sensitive volume '" << sensibleVolumeName
+                   << "' does not exist in geometry!!" << G4endl;
+            G4cout << "RESTG4 error. Please, review geometry! Press a key to crash!!" << G4endl;
+            getchar();
+            // We need to produce a clean exit at this point
+            exit(1);
+        }
     }
-    G4VPhysicalVolume* _vol = GetPhysicalVolume(SensVol);
+    G4VPhysicalVolume* _vol = GetPhysicalVolume(sensibleVolumeName);
 
     Double_t mx = restG4Metadata->GetMagneticField().X() * tesla;
     Double_t my = restG4Metadata->GetMagneticField().Y() * tesla;
@@ -78,7 +117,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     fieldMgr->SetDetectorField(magField);
     fieldMgr->CreateChordFinder(magField);
 
-    if (_vol != NULL) {
+    if (_vol != nullptr) {
         G4LogicalVolume* vol = _vol->GetLogicalVolume();
         // This method seems not available in my Geant4 version 10.4.2
         // In future Geant4 versions it seems possible to define field at particular volumes
@@ -90,7 +129,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
         G4cout << "Sensitivity volume temperature : " << mat->GetTemperature() << G4endl;
         G4cout << "Sensitivity volume density : " << mat->GetDensity() / (g / cm3) << " g/cm3" << G4endl;
     } else {
-        cout << "ERROR : Logical volume for sensitive \"" << SensVol << "\" not found!" << endl;
+        cout << "ERROR : Logical volume for sensitive \"" << sensibleVolumeName << "\" not found!" << endl;
     }
 
     // Getting generation volume
@@ -103,7 +142,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     // we should RETURN error
     if (type == "volume" && GenVol != "Not defined") {
         G4VPhysicalVolume* pVol = GetPhysicalVolume(GenVol);
-        if (pVol == NULL) {
+        if (pVol == nullptr) {
             cout << "ERROR : The generator volume was not found in the geometry" << endl;
             exit(1);
             return W;
@@ -163,23 +202,26 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     }
 
     for (int id = 0; id < restG4Metadata->GetNumberOfActiveVolumes(); id++) {
-        TString actVolName = restG4Metadata->GetActiveVolumeName(id);
-        G4VPhysicalVolume* pVol = GetPhysicalVolume((G4String)actVolName);
-        if (pVol != NULL) {
-            G4LogicalVolume* lVol = pVol->GetLogicalVolume();
-            if (restG4Metadata->GetMaxStepSize(actVolName) > 0) {
-                G4cout << "Setting maxStepSize = " << restG4Metadata->GetMaxStepSize(actVolName)
-                       << "mm for volume : " << actVolName << G4endl;
-                lVol->SetUserLimits(new G4UserLimits(restG4Metadata->GetMaxStepSize(actVolName) * mm));
-            }
-        }
+        TString activeVolumeName =
+            restG4Metadata->GetActiveVolumeName(id);  // this is the name of the physical volume
 
-        cout << "Activating volume : " << actVolName << endl;
-        restG4Event->AddActiveVolume((string)actVolName);
-        if (pVol == NULL) {
-            cout << "DetectorConstruction. Volume " << actVolName << " is not defined in the geometry"
-                 << endl;
-            exit(1);
+        G4LogicalVolumeStore* logicalVolumeStore = G4LogicalVolumeStore::GetInstance();
+        for (const auto& logicalVolume : *logicalVolumeStore) {
+            cout << "\t" << logicalVolume->GetName() << endl;
+            if (restG4Metadata->GetMaxStepSize(activeVolumeName) > 0) {
+                G4cout << "Setting maxStepSize = " << restG4Metadata->GetMaxStepSize(activeVolumeName)
+                       << "mm for volume : " << activeVolumeName << G4endl;
+                logicalVolume->SetUserLimits(
+                    new G4UserLimits(restG4Metadata->GetMaxStepSize(activeVolumeName) * mm));
+            }
+
+            cout << "Activating volume : " << activeVolumeName << endl;
+            restG4Event->AddActiveVolume((string)activeVolumeName);
+            if (logicalVolume == nullptr) {
+                cout << "Error: DetectorConstruction. Logical volume " << activeVolumeName
+                     << " is not defined in the geometry" << endl;
+                exit(1);
+            }
         }
     }
 
