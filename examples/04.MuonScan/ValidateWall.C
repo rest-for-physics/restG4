9c872ba2decbb5781715ecacefa1434b8590ec64
clang runner <javier.galan@unizar.es>
2021-07-08 18:40:08 +0200

    Adding a test for wall generator validation

---
+++ examples/04.MuonScan/ValidateWall.C
@@ -0,0 +1,35 @@
+
+Int_t ValidateWall(string fname) {
+    gSystem->Load("libRestFramework.so");
+    gSystem->Load("libRestGeant4.so");
+
+    TRestRun* run = new TRestRun(fname);
+    TRestGeant4Event* ev = (TRestGeant4Event*)run->GetInputEvent();
+
+    if (run->GetRunTag() != "CosmicsFromWall") {
+        cout << "Run tag : " << run->GetRunTag() << endl;
+        cout << "The run tag of the basic validation test should be 'MuonsFromWall!" << endl;
+        return 4;
+    }
+
+    Double_t r = 0;
+    for (Int_t n = 0; n < run->GetEntries(); n++) {
+        run->GetEntry(n);
+        Double_t x = ev->GetPrimaryEventOrigin().X();
+        Double_t y = ev->GetPrimaryEventOrigin().Y();
+
+        r += x * x + y * y;
+    }
+    r /= run->GetEntries();
+
+    if ((Int_t)(1000. * r) / 1000 < 10000 || (Int_t)(1000. * r) / 1000 > 20000) {
+        cout << "The average radius of the distribution is wrong!" << endl;
+        cout << "R: " << (Int_t)(1000. * r) / 1000 << endl;
+        return 5;
+    }
+
+    cout << "All tests passed! [\033[32mOK\033[0m]\n";
+    // Other tests like opening other metadata classes. Detector TGeoManager, etc.
+
+    return 0;
+}
