5b7cb787a87dd16f3fd56ee078c18577dd6afa59
lobis <luis.antonio.obis@gmail.com>
2022-09-05 21:47:32 +0200

    added more checks to validation

--- examples/13.IAXO/Validate.C
+++ examples/13.IAXO/Validate.C
@@ -21,9 +21,11 @@ Int_t Validate(const char* filename) {
     if (geometryInfo.GetAllPhysicalVolumes().size() != 374) {
         cout << "Incorrect number of physical volumes " << geometryInfo.GetAllPhysicalVolumes().size()
              << endl;
+        return 2;
     }
     if (geometryInfo.GetAllLogicalVolumes().size() != 22) {
         cout << "Incorrect number of logical volumes " << geometryInfo.GetAllLogicalVolumes().size() << endl;
+        return 3;
     }
 
     // event information
@@ -32,19 +34,43 @@ Int_t Validate(const char* filename) {
 
     if (event->GetNumberOfTracks() != 28059) {
         cout << "Incorrect number of tracks: " << event->GetNumberOfTracks() << endl;
+        return 4;
     }
     if (event->GetNumberOfHits() != 87189) {
         cout << "Incorrect number of hits: " << event->GetNumberOfHits() << endl;
+        return 5;
     }
 
     if (TMath::Abs(event->GetSensitiveVolumeEnergy() - 72.436883) > 1e-4) {
         cout << "Incorrect sensitive volume energy: " << event->GetSensitiveVolumeEnergy() << endl;
+        return 6;
     }
 
-    const auto scintillatorEnergy = event->GetEnergyInVolume(
-        "VetoSystem_vetoSystemBack_vetoLayerBack1_assembly-17.veto3_scintillatorVolume-800.0mm-f1a5df6c");
-    if (TMath::Abs(scintillatorEnergy - 1348.3876) > 1e-4) {
+    const auto scintillatorVolumeName =
+        "VetoSystem_vetoSystemBack_vetoLayerBack1_assembly-17.veto3_scintillatorVolume-800.0mm-f1a5df6c";
+    const auto scintillatorEnergy = event->GetEnergyInVolume(scintillatorVolumeName);
+    const auto scintillatorEnergyRef = 1348.3876;
+    if (TMath::Abs(scintillatorEnergy - scintillatorEnergyRef) > 1e-4) {
         cout << "Incorrect scintillator volume energy: " << scintillatorEnergy << endl;
+        return 7;
+    }
+
+    double scintillatorEnergyFromTracks = 0;
+    for (const auto& track : event->GetTracks()) {
+        const auto hits = track.GetHits();
+        for (int i = 0; i < hits.GetNumberOfHits(); i++) {
+            const auto volumeName = geometryInfo.GetVolumeFromID(hits.GetVolumeId(i));
+            if (volumeName != scintillatorVolumeName) {
+                continue;
+            }
+            const auto energy = hits.GetEnergy(i);
+            scintillatorEnergyFromTracks += energy;
+        }
+    }
+
+    if (TMath::Abs(scintillatorEnergyFromTracks - scintillatorEnergyRef) > 1e-4) {
+        cout << "Incorrect scintillator volume energy from tracks: " << scintillatorEnergyFromTracks << endl;
+        return 8;
     }
 
     cout << "All tests passed! [\033[32mOK\033[0m]\n";
