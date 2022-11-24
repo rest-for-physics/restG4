2c1eadb8454131e0d48c504bf3b91d76779bac1b
Luis Obis <luis.antonio.obis@gmail.com>
2022-11-24 12:13:42 +0100

    updated cosmic validation

--- examples/12.Generators/ValidateCosmicGenerator.C
+++ examples/12.Generators/ValidateCosmicGenerator.C
@@ -16,7 +16,7 @@ Int_t ValidateCosmicGenerator(const char* filename) {
     constexpr double tolerance = 0.1;
 
     TVector3 sourceDirection = metadata->GetParticleSource()->GetDirection();
-    if (TVector3(0, -1, 0).Angle(sourceDirection) > 0.01) {
+    if (TVector3(0.1, -0.5, 1.25).Angle(sourceDirection) > 0.01) {
         cout << "Bad source direction: (" << sourceDirection.X() << ", " << sourceDirection.Y() << ", "
              << sourceDirection.Z() << endl;
         return 3;
@@ -70,38 +70,38 @@ Int_t ValidateCosmicGenerator(const char* filename) {
     if (TMath::Abs(thetaAverage - thetaAverageRef) / thetaAverageRef > tolerance) {
         cout << "The average theta of the distribution is wrong!" << endl;
         cout << "thetaAverage (deg): " << thetaAverage << endl;
-        return 3;
+        return 5;
     }
     if (TMath::Abs(thetaMin - thetaMinRef) / (thetaMinRef + 1) > tolerance) {
         cout << "The minimum theta of the distribution is wrong!" << endl;
         cout << "thetaMin (deg): " << thetaMin << endl;
-        return 4;
+        return 6;
     }
     if (TMath::Abs(thetaMax - thetaMaxRef) / thetaMaxRef > tolerance) {
         cout << "The maximum theta of the distribution is wrong!" << endl;
         cout << "thetaMax (deg): " << thetaMax << endl;
-        return 5;
+        return 7;
     }
 
     if (TMath::Abs(energyPrimaryAverage - energyPrimaryAverageRef) / energyPrimaryAverageRef > tolerance) {
         cout << "The average energy of the distribution is wrong!" << endl;
         cout << "energyPrimaryAverage (keV): " << energyPrimaryAverage << endl;
-        return 6;
+        return 8;
     }
     if (TMath::Abs(energyPrimaryMin - energyPrimaryMinRef) / energyPrimaryMinRef > tolerance) {
         cout << "The minimum energy of the distribution is wrong!" << endl;
         cout << "energyPrimaryMin (keV): " << energyPrimaryMin << endl;
-        return 7;
+        return 9;
     }
     if (TMath::Abs(energyPrimaryMax - energyPrimaryMaxRef) / energyPrimaryMaxRef > tolerance) {
         cout << "The maximum energy of the distribution is wrong!" << endl;
         cout << "energyPrimaryMax (keV): " << energyPrimaryMax << endl;
-        return 8;
+        return 10;
     }
 
     if (metadata->GetNumberOfEvents() != 1E6) {
         cout << "wrong number of events: " << metadata->GetNumberOfEvents() << endl;
-        return 9;
+        return 11;
     }
 
     const auto surfaceTermRef = 942.4778;
@@ -111,19 +111,19 @@ Int_t ValidateCosmicGenerator(const char* filename) {
         tolerance) {
         cout << "wrong cosmic surface term: "
              << metadata->GetGeant4PrimaryGeneratorInfo().GetSpatialGeneratorCosmicSurfaceTermCm2() << endl;
-        return 10;
+        return 12;
     }
 
     const auto cosmicFluxRef = 0.108215;
     if (abs(metadata->GetCosmicFluxInCountsPerCm2PerSecond() - cosmicFluxRef) / cosmicFluxRef > tolerance) {
         cout << "wrong cosmic flux: " << metadata->GetCosmicFluxInCountsPerCm2PerSecond() << endl;
-        return 11;
+        return 13;
     }
 
     const auto simulationTimeRef = 9804.87;
     if (abs(metadata->GetEquivalentSimulatedTime() - simulationTimeRef) / simulationTimeRef > tolerance) {
         cout << "wrong equivalent simulation time: " << metadata->GetEquivalentSimulatedTime() << endl;
-        return 12;
+        return 14;
     }
 
     cout << "All tests passed! [\033[32mOK\033[0m]\n";
