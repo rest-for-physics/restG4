e4566d29dd42c4158df94aa94ca5e9606f19bb1c
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-05-03 09:19:57 +0200

    update macro and settings

--- examples/15.DecaySpectra/GenerateTrees.C
+++ examples/15.DecaySpectra/GenerateTrees.C
@@ -15,23 +15,35 @@ void GenerateTrees(const char* filename) {
     // Create a new tree to store gammas from the primary particle
     TFile* f = new TFile(TString::Format("ParticlesFrom%sDecay.root", primaryName.Data()), "RECREATE");
 
-    const vector<string> particlesToRecord = {"gamma", "e-", "e+", "alpha"};
+    // store particles and name of the root TTree
+    const map<string, string> particleToString = {
+        {"gamma", "photons"},  {"e-", "electrons"},     {"e+", "positrons"},
+        {"proton", "protons"}, {"neutron", "neutrons"}, {"alpha", "alphas"},
+    };
+    vector<string> particlesToRecord;
+    for (const auto& particle : particleToString) {
+        particlesToRecord.push_back(particle.first);
+    }
     // create a new tree for each particle
     map<string, TTree*> trees;
     for (const auto& particle : particlesToRecord) {
         // we cannot use the particle name (e.g. gamma) as a tree name, because it is a reserved word
-        trees[particle] = new TTree(TString::Format("decay_%s", particle.c_str()),
-                                    TString::Format("%s particles", particle.c_str()));
+        cout << "Creating tree for " << particle << " with name " << particleToString.at(particle) << endl;
+        trees[particle] =
+            new TTree(particleToString.at(particle).c_str(), particleToString.at(particle).c_str());
     }
 
     // create a branch for each particle
     map<string, double> energies;
     for (const auto& particle : particlesToRecord) {
+        cout << "Creating branch for " << particle << endl;
         energies[particle] = 0;
         trees[particle]->Branch("energy", &energies[particle], "energy/D");
     }
 
     const string detectorName = "detector";
+    cout << "Detector name: " << detectorName << endl;
+    cout << "Starting loop over events..." << endl;
     for (int n = 0; n < run.GetEntries(); n++) {
         // print the progress every 1%
         if (n % (run.GetEntries() / 100) == 0) {
