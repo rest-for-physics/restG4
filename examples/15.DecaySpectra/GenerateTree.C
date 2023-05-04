fd0a19fc5fcca940f4e60cda74bb334bc4755db7
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-05-04 10:54:10 +0200

    update tree generation code, add code to produce histogram

--- examples/15.DecaySpectra/GenerateTree.C
+++ examples/15.DecaySpectra/GenerateTree.C
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void GenerateTrees(const char* filenameOrDirectoryWithFiles) {
+void GenerateTree(const char* filenameOrDirectoryWithFiles) {
     gSystem->Load("libRestFramework.so");
     gSystem->Load("libRestGeant4.so");
 
@@ -11,10 +11,29 @@ void GenerateTrees(const char* filenameOrDirectoryWithFiles) {
     if (filesystem::is_directory(filenameOrDirectoryWithFiles)) {
         for (const auto& entry : filesystem::directory_iterator(filenameOrDirectoryWithFiles)) {
             filenames.push_back(entry.path());
+            cout << "Adding file: " << entry.path() << endl;
         }
     } else {
         filenames.push_back(filenameOrDirectoryWithFiles);
     }
+
+    TFile* outputFile = nullptr;
+    const map<string, string> particleToString = {
+        {"gamma", "gammas"},
+        /*
+        {"e-",      "electrons"},
+        {"e+",      "positrons"},
+        {"proton",  "protons"},
+        {"neutron", "neutrons"},
+        {"alpha",   "alphas"},
+         */
+    };
+    vector<string> particlesToRecord;
+    for (const auto& particle : particleToString) {
+        particlesToRecord.push_back(particle.first);
+    }
+    map<string, TTree*> trees;
+    map<string, double> energies;
 
     bool first = true;
     for (const auto& filename : filenames) {
@@ -26,24 +45,6 @@ void GenerateTrees(const char* filenameOrDirectoryWithFiles) {
         TRestGeant4Event* event = run.GetInputEvent<TRestGeant4Event>();
         run.GetEntry(0);
 
-        TFile* outputFile = nullptr;
-        const map<string, string> particleToString = {
-            {"gamma", "gammas"},
-            /*
-            {"e-",      "electrons"},
-            {"e+",      "positrons"},
-            {"proton",  "protons"},
-            {"neutron", "neutrons"},
-            {"alpha",   "alphas"},
-             */
-        };
-        vector<string> particlesToRecord;
-        for (const auto& particle : particleToString) {
-            particlesToRecord.push_back(particle.first);
-        }
-        map<string, unique_ptr<TTree>> trees;
-        map<string, double> energies;
-
         if (first) {
             const TString primaryName = event->GetPrimaryEventParticleName();
             // Create a new tree to store gammas from the primary particle
@@ -53,8 +54,8 @@ void GenerateTrees(const char* filenameOrDirectoryWithFiles) {
             // create a new tree for each particle
             for (const auto& particle : particlesToRecord) {
                 // we cannot use the particle name (e.g. gamma) as a tree name, because it is a reserved word
-                trees[particle] = make_unique<TTree>(particleToString.at(particle).c_str(),
-                                                     particleToString.at(particle).c_str());
+                trees[particle] =
+                    new TTree(particleToString.at(particle).c_str(), particleToString.at(particle).c_str());
             }
 
             // create a branch for each particle
@@ -95,8 +96,31 @@ void GenerateTrees(const char* filenameOrDirectoryWithFiles) {
                 }
             }
         }
-
-        outputFile->Write();
-        outputFile->Close();
     }
+
+    outputFile->Write();
+    outputFile->Close();
+
+    /*
+     * Histograms can be merged into a single file using the following root code:
+
+TFile* U238 = TFile::Open("U238/gammas.root");
+TFile* Th232 = TFile::Open("Th232/gammas.root");
+
+TH1D* U238h = U238->Get<TH1D>("histogram");
+TH1D* Th232h = Th232->Get<TH1D>("histogram");
+
+// rename histograms
+U238h->SetName("U238");
+Th232h->SetName("Th232");
+
+// write to file
+TFile* outputFile = new TFile("merged.root", "RECREATE");
+
+U238h->Write();
+Th232h->Write();
+
+outputFile->Close();
+
+     */
 }
