55df05f592ffec682a0d0387a98b292e14c24877
Álvaro <110617994+AlvaroEzq@users.noreply.github.com>
2026-01-22 13:47:32 +0100

    Differentiate between identical volumes (#142)
    * filter sensitive physical volumes
    
    * save the step info with the correct particular physical volume
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * store physical volumes rotation in world frame
    
    * fix primary generator for nested volumes and fix rotation in GetPositionOnGDMLVolume and GetPositionOnGDMLSurface
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix world volume as sensitive and active volume
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * exclude daughters volumes from primary generation
    
    * save also the rotation into TRestGeant4PrimaryGeneratorInfo
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/SensitiveDetector.cxx
+++ src/SensitiveDetector.cxx
@@ -16,10 +16,41 @@ SensitiveDetector::SensitiveDetector(SimulationManager* simulationManager, const
 
 G4bool SensitiveDetector::ProcessHits(G4Step* step, G4TouchableHistory*) {
     // return value will always be ignored, its present for backwards compatibility (I guess)
-    const auto volumeName = fSimulationManager->GetRestMetadata()
-                                ->GetGeant4GeometryInfo()
-                                .GetAlternativeNameFromGeant4PhysicalName(
-                                    step->GetPreStepPoint()->GetPhysicalVolume()->GetName());
+    TRestGeant4Metadata* restG4Metadata = fSimulationManager->GetRestMetadata();
+    const auto& geometryInfo = restG4Metadata->GetGeant4GeometryInfo();
+    const auto volumeName = geometryInfo.GetAlternativeNameFromGeant4PhysicalName(
+        step->GetPreStepPoint()->GetPhysicalVolume()->GetName());
+
+    /* Sensitive detector are assigned to logical volumes, but several physical volumes
+    can share the same logical volume. Now we get the physical volume where the pre-step
+    is located and filter if it is one of the declared sensitive volumes by the user*/
+
+    // Get the full name (path) of the physical volume which uniquely identifies it
+    auto th = step->GetPreStepPoint()->GetTouchable();
+    G4int depth = th->GetHistoryDepth();
+    G4String geant4path = "";
+    if (depth == 0) {  // it is the world volume
+        geant4path = th->GetVolume()->GetName();
+    }
+    for (G4int i = 1; i <= depth; ++i) {  // start from 1 to skip world volume
+        // Move the touchable to level i (0 = current volume, depth = world)
+        G4VPhysicalVolume* pv = th->GetVolume(depth - i);
+        if (pv) {
+            if (geant4path != "") {
+                geant4path += geometryInfo.GetPathSeparator().Data();
+            }
+            geant4path += pv->GetName();
+        }
+    }
+    // convert to the names used in gdml (due to assemblies)
+    const auto physicalFullName = geometryInfo.GetAlternativePathFromGeant4Path(geant4path);
+
+    // search and filter if this physical volume is sensitive
+    auto sensitiveVolumes = restG4Metadata->GetSensitiveVolumes();
+    if (std::find(sensitiveVolumes.begin(), sensitiveVolumes.end(), physicalFullName) ==
+        sensitiveVolumes.end()) {
+        return false;
+    }
 
     const bool isGeantino = step->GetTrack()->GetParticleDefinition() == G4Geantino::Definition();
 
